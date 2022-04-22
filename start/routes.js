1a236599313677b90953cfd43d15dc3b5c58044d
Sotatek-TanHoang <100083240+Sotatek-TanHoang@users.noreply.github.com>
2022-04-22 16:25:47 +0700

    comment on admin routes

--- start/routes.js
+++ start/routes.js
@@ -23,31 +23,38 @@ Route.get('image/:fileName', 'FileController.getImage');
 Route.group(() => {
   // Auth
   Route.post('/login', 'AuthAdminController.login').validator('Login')
-  // .middleware('checkSignature');
+  // comment line below to bypass signature checking.
+  .middleware('checkSignature');
 
   // TODO: implement confirm email later
 }).prefix(Const.USER_TYPE_PREFIX.ADMIN).middleware(['typeAdmin', 'checkPrefix', 'formatEmailAndWallet']);
 
-// Admin only work route
+// Admin only work routes
 Route.group(() => {
 
-
+  // create admin or governance by admin.
   Route.post('/create-admin', 'AdminController.create').validator('CreateAdmin').middleware('checkParamRole');
+  // get list of admins with pagination.
   Route.get('admins', 'AdminController.adminList');
+  // get single admin profile by id.
   Route.get('admins/:id', 'AdminController.adminDetail');
+  // update single admin by id.
   Route.put('/update-admin/:id','AdminController.update').validator('UpdateAdmin').middleware('checkParamRole');
+  // delete single admin by id.
   Route.delete('/delete-admin/:id','AdminController.delete')
+  // unused.
   Route.get('check-wallet-address', 'AuthAdminController.checkWalletAddress');
   Route.post('check-wallet-address', 'AuthAdminController.checkWalletAddress');
 
 }).prefix(Const.USER_TYPE_PREFIX.ADMIN)
 .middleware(['typeAdmin', 'checkPrefix', 'checkAdminJwtSecret', 'checkAdminOnly', 'auth:admin']);
 
-// Admin and Governance proposal route
+// Admin and Governance proposal routes
 Route.group(() => {
 
   // Route.post('create-proposal', '...handler');
   // Route.post('update-proposal/:id', '...handler');
+  Route.get('/test', () => 'It\'s working')
 
 }).prefix(Const.USER_TYPE_PREFIX.ADMIN).middleware(['typeAdmin', 'checkPrefix', 'checkAdminJwtSecret',
   'auth:admin'
