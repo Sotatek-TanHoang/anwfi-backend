40d7ad0166b4472f1dee16e3e6b5a79f3a4b865f
Sotatek-TanHoang <tan.hoang@sotatek.com>
2022-04-27 17:40:15 +0700

    add unique constrant for vote model

--- database/migrations/1651042703089_vote_schema.js
+++ database/migrations/1651042703089_vote_schema.js
@@ -7,10 +7,11 @@ class VoteSchema extends Schema {
   up () {
     this.create('votes', (table) => {
       table.increments()
-      table.string('address', 255).notNullable();
+      table.string('wallet_address', 255).notNullable();
       table.integer('proposal_id').notNullable();
       table.bool('vote',65,0).notNullable();
       table.timestamps();
+      table.unique(['wallet_address']);
     })
   }
 
