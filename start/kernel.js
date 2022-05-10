885f04fb9a37fd91d4a66b27f28ce4b8473273ca
Sotatek-TanHoang <tan.hoang@sotatek.com>
2022-05-10 09:57:50 +0700

    fix typo continue

--- start/kernel.js
+++ start/kernel.js
@@ -55,7 +55,7 @@ const namedMiddleware = {
   checkUserSignature: 'App/Middleware/CheckUserSignature',
   checkAdminSignature: 'App/Middleware/CheckAdminSignature',
   checkBlockPassSignature: 'App/Middleware/CheckBlockPassSignature',
-  checkGovernanceAbove:'App/Middleware/checkGovernanceAbove',
+  checkGovernanceAbove:'App/Middleware/CheckGovernanceAbove',
   formatEmailAndWallet: 'App/Middleware/FormatEmailAndWallet',
   maskEmailAndWallet: 'App/Middleware/MaskEmailAndWallet',
   typeAdmin: 'App/Middleware/TypeAdmin',
