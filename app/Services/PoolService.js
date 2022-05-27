fe31c392e24f29747ae84e8656432b0954779e05
Sotatek-TanHoang <tan.hoang@sotatek.com>
2022-05-27 10:06:40 +0700

    edit check stake_token check except current pool id

--- app/Services/PoolService.js
+++ app/Services/PoolService.js
@@ -15,6 +15,9 @@ class PoolService {
     if (params.id) {
       builder = builder.where('id', params.id);
     }
+    if(params.except){
+      builder = builder.where('id','!=', params.except);
+    }
     if (params.pool_index) {
       builder = builder.where('pool_index', params.pool_index);
     }
